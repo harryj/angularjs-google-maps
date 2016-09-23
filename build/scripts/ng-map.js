19c039498857b9dce00c04c25be50bd0dd60a5a3
harry jiang <flamingfurball89@yahoo.com>
2016-09-23 10:22:54 -0700

    fixed

--- build/scripts/ng-map.js
+++ build/scripts/ng-map.js
@@ -520,9 +520,10 @@ angular.module('ngMap', []);
       position && (this.position = position); /* jshint ignore:line */
 
       if (this.getProjection() && typeof this.position.lng == 'function') {
-        var posPixel = this.getProjection().fromLatLngToDivPixel(this.position);
+        
         var _this = this;
         var setPosition = function() {
+          var posPixel = this.getProjection().fromLatLngToDivPixel(this.position);
           var x = Math.round(posPixel.x - (_this.el.offsetWidth/2));
           var y = Math.round(posPixel.y - _this.el.offsetHeight - 10); // 10px for anchor
           _this.el.style.left = x + "px";
